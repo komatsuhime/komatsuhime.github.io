3176cf8e71c98e407e2780a7d393988a65938a8c
Carla <carla.faroldi@gmail.com>
2023-07-15 16:54:18 -0400

    Add color picker

--- project003/script.js
+++ project003/script.js
@@ -1,4 +1,10 @@
 const squares = document.querySelectorAll('.row');
+const colorInput = document.querySelector("#color");
+let color = "#fff";
+
+colorInput.addEventListener('input', function(e) {
+    color = colorInput.value;
+});
 
 for (let square of squares) {
     square.addEventListener("click", toggleClass);
@@ -8,8 +14,10 @@ function toggleClass() {
     if (this.classList.contains('erase')) {
         this.classList.remove('erase');
         this.classList.add('draw');
+        this.style.backgroundColor = `${color}`;
     } else if (this.classList.contains('draw'))  {
         this.classList.remove('draw');
         this.classList.add('erase');
+        this.style.backgroundColor = "#fff";
     }
 }
