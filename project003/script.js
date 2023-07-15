e42de4177c5a4de2b69e7817eea81537827c5dc9
Carla <carla.faroldi@gmail.com>
2023-07-15 17:10:21 -0400

    Make it work on mobile

--- project003/script.js
+++ project003/script.js
@@ -4,7 +4,6 @@ let color = "#000";
 
 colorInput.addEventListener('input', function(e) {
     color = colorInput.value;
-    console.log(color);
 });
 
 for (let square of squares) {
@@ -16,7 +15,6 @@ function toggleClass() {
         this.classList.remove('erase');
         this.classList.add('draw');
         this.style.backgroundColor = `${color}`;
-        console.log('click!')
     } else if (this.classList.contains('draw'))  {
         this.classList.remove('draw');
         this.classList.add('erase');
