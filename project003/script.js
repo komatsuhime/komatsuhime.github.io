5b92fc65fe0b2379beafc05de36f7607427a36b7
Carla <carla.faroldi@gmail.com>
2023-07-15 17:09:17 -0400

    Try new things

--- project003/script.js
+++ project003/script.js
@@ -4,6 +4,7 @@ let color = "#000";
 
 colorInput.addEventListener('input', function(e) {
     color = colorInput.value;
+    console.log(color);
 });
 
 for (let square of squares) {
@@ -15,6 +16,7 @@ function toggleClass() {
         this.classList.remove('erase');
         this.classList.add('draw');
         this.style.backgroundColor = `${color}`;
+        console.log('click!')
     } else if (this.classList.contains('draw'))  {
         this.classList.remove('draw');
         this.classList.add('erase');
